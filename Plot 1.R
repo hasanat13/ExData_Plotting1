6c91a42235ee4707d6125b42fc380c7356497483
hasanat13 <47308079+hasanat13@users.noreply.github.com>
2019-04-30 23:21:59 +0700

    Update Plot 1.R

--- Plot 1.R
+++ Plot 1.R
@@ -1,16 +1,11 @@
 #reading data
-data_hh <- read.delim('household_power_consumption.txt',sep=';')
+data_hh <- read.table('household_power_consumption.txt',header=T, sep=';', na.strings="?")
 
-#Convert data from factor to numeric 
-data_hh$Sub_metering_1 <- as.numeric(as.factor(data_hh$Sub_metering_1))
-data_hh$Sub_metering_2 <- as.numeric((as.factor(data_hh$Sub_metering_2))
-data_hh$Global_active_power <- as.numeric(as.factor(data_hh$Global_active_power))
-data_hh$Voltage<- as.numeric(as.factor(data_hh$Voltage))
-data_hh$Global_reactive_power<- as.numeric((as.factor(data_hh$Global_reactive_power))
-data_hh$Global_intensity <- as.numeric(as.factor(data_hh$Global_intensity))
+#Convert data from factor to date
+data_hh$Date <- as.Date(data_hh$Date, format = "%d/%m/%Y")
 
 #Subsetting data (1/2/2007 until 2/2/2007)
-subsetdate <- subset(data_hh,data_hh$Date==c("1/2/2007","2/2/2007"))
+subsetdata <- subset(data_hh,Date>="2007-02-01" & Date<="2007-02-02")
 
 #Plot_1
-hist(subsetdata$Global_active_power,col="red",main="Global Active Power",xlab= "Global Active Power (Kilowatts)")
+hist(subsetdata$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (Kilowatts)")
